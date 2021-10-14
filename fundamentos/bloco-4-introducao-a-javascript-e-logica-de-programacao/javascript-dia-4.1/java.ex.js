e07ea14759779f0e59a32dd02e3b1b6cfdc3a01d
yang vieira <yangflix3@gmail.com>
2021-10-13 23:44:55 -0400

    ex4.1

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/javascript-dia-4.1/java.ex.js
@@ -0,0 +1,128 @@
+// 1.Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b , definidas no começo com os valores que serão operados. Faça programas para:
+// const a = 5;
+// const b = 6;
+// let result;
+// console.log(result = a * b)
+// console.log(result = a + b)
+// console.log(result = a - b)
+// console.log(result = a ** b)
+// console.log(result = a % b)
+// console.log(result = a / b)
+// 2.Faça um programa que retorne o maior de dois números. Defina no começo do programa duas constantes com os valores que serão comparados.
+// const a = 20;
+// const b = 30;
+// if(a > b) {
+// console.log(a);
+// }else{
+//     console.log(b)
+
+// 3-Faça um programa que retorne o maior de três números. Defina no começo do programa três constantes com os valores que serão comparados.
+// const a = 30;
+// const b = 40;
+// const c = 50;
+// if(a > b && a > c) {
+// console.log(a)
+// } else if(b > c) {
+//     console.log(b)
+// } else {
+//     console.log(c)
+// }
+// 4- Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+// const valor = 5
+// if(5 > 0) {
+//     console.log("positive")
+// }else {
+//     console.log("negative")
+// }
+// 5 - Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
+// 
+// 6 -Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
+// Como desafio, faça o programa funcionar tanto se receber o nome de uma peça com letras maiúsculas quanto com letras minúsculas, sem aumentar a quantidade de condicionais.
+// Como dica, você pode pesquisar uma função que faz uma string ficar com todas as letras minúsculas (lower case) .
+// Se a peça passada for inválida, o programa deve retornar uma mensagem de erro.
+// Exemplo: bishop (bispo) -> diagonals (diagonais)
+// const peçaXadrez = "RAINHA"
+// console.log(peçaXadrez)
+// const peça = peçaXadrez.toLowerCase()
+// console.log(peça)
+// switch(peça) {
+//     case"bispo":
+//     console.log("diagonal")
+//     break;
+//     case "rei":
+//     console.log ("vertical, horizontal, apenas uma casa")
+//     break;
+//     case "rainha":
+//     console.log("TODOS OS LADOS QUE ELA QUISER")
+//}
+//Escreva um programa que converta uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga essas regras:
+// Porcentagem >= 90 -> A
+// Porcentagem >= 80 -> B
+// Porcentagem >= 70 -> C
+// Porcentagem >= 60 -> D
+// Porcentagem >= 50 -> E
+// Porcentagem < 50 -> F
+// O programa deve retornar uma mensagem de erro e encerrar se a nota passada for menor que 0 ou maior que 100.
+
+// let nota = 80;
+// if(nota < 0 || nota > 100) {
+//     console.log("nota invalida")
+// }else if(nota >= 90) {
+//     console.log("A")
+// }else if(nota >= 80) {
+//     console.log("B")
+// }else if(nota >= 70) {
+//     console.log("C") 
+// }else if(nota >= 60) {
+//      console.log("D")
+// }else if(nota >= 50){
+// console.log("E");
+// }else{
+//     console.log("F")
+// }
+// 8=Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false .
+//const a = 7;
+//const b = 5;
+//const c = 1;
+//let valor = false
+//if(a % 2 === 0 || b % 2 === 0 || c % 2 === 0) {
+   // valor = true
+//}
+//console.log(valor)
+// 9-Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false .
+// const a = 10
+// const b = 20
+// const c = 30
+// if(a % 2 === 0 || b % 2 === 0 || c % 2 === 0 ) {
+//    console.log(true)
+// }else{
+//    console.log(false)
+// }
+
+//10- Escreva um programa que se inicie com dois valores em duas constantes diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos.
+// Atente que, sobre o custo do produto, incide um imposto de 20%.
+// Seu programa também deve emitir uma mensagem de erro e encerrar caso algum dos seus valores de entrada seja menor que zero.
+// O lucro de um produto é o resultado da subtração do valor de venda pelo custo do mesmo, sendo que o imposto de 20% também faz parte do valor de custo.
+// valorCustoTotal = valorCusto + impostoSobreOCusto
+// lucro = valorVenda - valorCustoTotal (lucro de um produto)
+// const custo = 5.50;
+// const preco = 10.50;
+// let quantidadeProdutos = 1000;
+//  let custoTotal = custo * 1.2;
+//  let lucro = preco - custoTotal;
+
+// console.log(lucro);
+//  if(custo < 0 && preco <0) {
+//     console.log("erro")
+//  } else {
+//     console.log(lucro * quantidadeProdutos)
+//  }
+
+
+
+
+
+
+
+
+

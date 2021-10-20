aa40a5718341ad12bbd021149e7cc2314dcc7360
yang vieira <yangflix3@gmail.com>
2021-10-19 20:52:57 -0400

    fazendo os ex

---
+++ fundamentos/bloco-5-dom/dom.js
@@ -0,0 +1,4 @@
+const mudando =  document.getElementById("paragraph").innerText = 'la casa de palpel'
+ document.getElementById("paragraph").style.backgroundColor = 'red'
+document.getElementById("second-paragraph").innerText = 'minha serie pre ferida'
+document.getElementById("subtitle").innerText = 'a chipa ta 1$ reallll'

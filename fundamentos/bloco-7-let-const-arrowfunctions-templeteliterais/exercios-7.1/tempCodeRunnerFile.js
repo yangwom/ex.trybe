9870b0b7b6531f3c5151ade0b191a824e2484bfe
yang vieira <yangflix3@gmail.com>
2021-11-11 14:18:04 -0400

    ex 7.1 arrow functions

---
+++ fundamentos/bloco-7-let-const-arrowfunctions-templeteliterais/exercios-7.1/tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+const factorial = number => {
+//         let resultado = 1;
+//         for (let i = 1; i <= number; i++) {
+//             resultado *= i
+//         }
+       
+//         return resultado;
+//     }
+
+// console.log(factorial(4));
